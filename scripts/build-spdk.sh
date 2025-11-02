2d6b97fc00415e16464605a41a3d6d140b865aea
Derek Su <derek.su@suse.com>
2025-11-02 20:06:27 +0800

    chore(spdk): update python311 to python3
    Longhorn 11975
    
    Signed-off-by: Derek Su <derek.su@suse.com>

--- scripts/build-spdk.sh
+++ scripts/build-spdk.sh
@@ -34,10 +34,6 @@ cd "$SRC_DIR"
 git checkout "$SPDK_COMMIT_ID"
 git submodule update --init
 
-# Modify package dependency script for SLES
-sed -i '/python3-pyelftools/d' ./scripts/pkgdep/sles.sh
-sed -i 's/python3-/python311-/g' ./scripts/pkgdep/sles.sh
-
 # Install dependencies
 ./scripts/pkgdep.sh --uring
 pip3 install -r ./scripts/pkgdep/requirements.txt
